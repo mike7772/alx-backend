315ef68857008f4fba965f4cdb89285e222747a9
mike7772 <michaelasnake644@gmail.com>
2022-08-17 19:41:27 +0000

    redis

---
+++ 0x03-queuing_system_in_js/6-job_processor.js
@@ -0,0 +1,34 @@
+import kue from 'kue';
+
+const queue = kue.createQueue();
+
+function sendNotification(phoneNumber, message) {
+  console.log(
+    `Sending notification to ${phoneNumber}, with message: ${message}`
+  );
+}
+
+const queueName = 'push_notification_code';
+
+queue.process(queueName, (job, done) => {
+  const { phoneNumber, message } = job.data;
+  sendNotification(phoneNumber, message);
+  done();
+});
+import kue from 'kue';
+
+const queue = kue.createQueue();
+
+function sendNotification(phoneNumber, message) {
+  console.log(
+    `Sending notification to ${phoneNumber}, with message: ${message}`
+  );
+}
+
+const queueName = 'push_notification_code';
+
+queue.process(queueName, (job, done) => {
+  const { phoneNumber, message } = job.data;
+  sendNotification(phoneNumber, message);
+  done();
+});
